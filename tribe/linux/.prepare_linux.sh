0d36f2b9ee8d5948d9a9273a494e4956455a90d9
Mike Reznikov <mirekez@github.com>
2026-05-31 11:36:06 +0000

    fixing linux 6.19 dts

--- tribe/linux/.prepare_linux.sh
+++ tribe/linux/.prepare_linux.sh
@@ -10,6 +10,11 @@ TRIBE_SD_DRIVER="${TRIBE_SD_DRIVER:-${LINUX_DIR}/tribe_sd.c}"
 RISCV_HOME="${RISCV_HOME:-/home/me/riscv}"
 CROSS_COMPILE="${CROSS_COMPILE:-${RISCV_HOME}/bin/riscv32-unknown-linux-gnu-}"
 JOBS="${JOBS:-$(nproc 2>/dev/null || echo 4)}"
+TRIBE_CPU_CLOCK_HZ="${TRIBE_CPU_CLOCK_HZ:-50000000}"
+TRIBE_CLINT_TICK_DIV="${TRIBE_CLINT_TICK_DIV:-256}"
+TRIBE_TIMEBASE_HZ="${TRIBE_TIMEBASE_HZ:-$((TRIBE_CPU_CLOCK_HZ / TRIBE_CLINT_TICK_DIV))}"
+TRIBE_LINUX_BAUD="${TRIBE_LINUX_BAUD:-1000000}"
+TRIBE_LINUX_BOOTARGS="${TRIBE_LINUX_BOOTARGS:-console=ttyS0,${TRIBE_LINUX_BAUD} earlycon=uart,mmio,0x82000000 unaligned_scalar_speed=slow}"
 
 usage()
 {
@@ -85,6 +90,118 @@ kernel_make()
     make -C "${KERNEL_SRC}" "${MAKE_ARGS[@]}" "$@"
 }
 
+write_tribe_dts()
+{
+    python3 - "${LINUX_DIR}/config32.dts" "${TRIBE_LINUX_BOOTARGS}" "${TRIBE_LINUX_BAUD}" "${TRIBE_TIMEBASE_HZ}" "${TRIBE_CPU_CLOCK_HZ}" <<'PY'
+import pathlib
+import sys
+
+path = pathlib.Path(sys.argv[1])
+bootargs = sys.argv[2]
+baud = int(sys.argv[3], 0)
+timebase = int(sys.argv[4], 0)
+cpu_clock = int(sys.argv[5], 0)
+
+path.write_text(f"""/dts-v1/;
+
+/ {{
+\tmodel = "CppHDL Tribe";
+\t#address-cells = <1>;
+\t#size-cells = <1>;
+\tcompatible = "cpphdl,tribe";
+
+\taliases {{
+\t\tserial0 = &uart0;
+\t}};
+
+\tchosen {{
+\t\tbootargs = "{bootargs}";
+\t\tstdout-path = "/soc/serial@82000000:{baud}n8";
+\t}};
+
+\tcpus {{
+\t\t#address-cells = <1>;
+\t\t#size-cells = <0>;
+\t\ttimebase-frequency = <{timebase}>;
+
+\t\tcpu@0 {{
+\t\t\tdevice_type = "cpu";
+\t\t\treg = <0>;
+\t\t\tstatus = "okay";
+\t\t\tcompatible = "riscv";
+\t\t\triscv,isa = "rv32imac";
+\t\t\triscv,isa-base = "rv32i";
+\t\t\triscv,isa-extensions = "i", "m", "a", "c";
+\t\t\tmmu-type = "riscv,sv32";
+\t\t\tclock-frequency = <{cpu_clock}>;
+
+\t\t\tcpu_intc: interrupt-controller {{
+\t\t\t\t#interrupt-cells = <1>;
+\t\t\t\tinterrupt-controller;
+\t\t\t\tcompatible = "riscv,cpu-intc";
+\t\t\t}};
+\t\t}};
+\t}};
+
+\tmemory@80000000 {{
+\t\tdevice_type = "memory";
+\t\treg = <0x80000000 0x02000000>;
+\t}};
+
+\tsoc {{
+\t\t#address-cells = <1>;
+\t\t#size-cells = <1>;
+\t\tcompatible = "simple-bus";
+\t\tranges;
+
+\t\tuart0: serial@82000000 {{
+\t\t\tcompatible = "ns16550a";
+\t\t\treg = <0x82000000 0x100>;
+\t\t\tclock-frequency = <{cpu_clock}>;
+\t\t\tcurrent-speed = <{baud}>;
+\t\t\treg-shift = <0>;
+\t\t\treg-io-width = <1>;
+\t\t\tfifo-size = <4096>;
+\t\t\tinterrupt-parent = <&plic0>;
+\t\t\tinterrupts = <1>;
+\t\t\tstatus = "okay";
+\t\t}};
+
+\t\tclint0: clint@82000100 {{
+\t\t\tcompatible = "sifive,clint0", "riscv,clint0";
+\t\t\treg = <0x82000100 0x0000c000>;
+\t\t\tinterrupts-extended = <&cpu_intc 3 &cpu_intc 7>;
+\t\t}};
+
+\t\tsd0: sd@8200d100 {{
+\t\t\tcompatible = "cpphdl,tribe-sd";
+\t\t\treg = <0x8200d100 0x100>;
+\t\t\tinterrupt-parent = <&plic0>;
+\t\t\tinterrupts = <2>;
+\t\t\ttribe,capacity-sectors = <131072>;
+\t\t\tstatus = "okay";
+\t\t}};
+
+\t\tplic0: interrupt-controller@82010000 {{
+\t\t\t#interrupt-cells = <1>;
+\t\t\tinterrupt-controller;
+\t\t\tcompatible = "riscv,plic0";
+\t\t\treg = <0x82010000 0x00210000>;
+\t\t\triscv,ndev = <31>;
+\t\t\tinterrupts-extended = <&cpu_intc 9>;
+\t\t}};
+\t}};
+}};
+""", encoding="utf-8")
+PY
+
+    if command -v "${DTC:-dtc}" >/dev/null 2>&1; then
+        "${DTC:-dtc}" -I dts -O dtb -o "${LINUX_DIR}/config32.dtb" "${LINUX_DIR}/config32.dts"
+    else
+        echo "warning: missing dtc, leaving ${LINUX_DIR}/config32.dtb for run_linux_probe.sh to rebuild" >&2
+    fi
+}
+
 patch_tribe_sd_driver()
 {
     cp "${TRIBE_SD_DRIVER}" "${KERNEL_SRC}/drivers/block/tribe_sd.c"
@@ -123,35 +240,6 @@ if line not in text:
         raise SystemExit("could not find BLK_DEV_LOOP insertion point in drivers/block/Makefile")
     text = text.replace(needle, line + needle, 1)
     path.write_text(text)
-PY
-}
-
-patch_nbcon_default_prio()
-{
-    python3 - "${KERNEL_SRC}/kernel/printk/nbcon.c" <<'PY'
-import pathlib
-import re
-import sys
-
-path = pathlib.Path(sys.argv[1])
-text = path.read_text()
-pattern = re.compile(
-    r"enum nbcon_prio nbcon_get_default_prio\(void\)\n"
-    r"\{\n"
-    r".*?"
-    r"\n\}\n",
-    re.S,
-)
-replacement = """enum nbcon_prio nbcon_get_default_prio(void)
-{
-\treturn NBCON_PRIO_NORMAL;
-}
-"""
-updated, count = pattern.subn(replacement, text, count=1)
-if count != 1:
-    raise SystemExit("could not patch nbcon_get_default_prio")
-if updated != text:
-    path.write_text(updated)
 PY
 }
 
@@ -216,7 +304,7 @@ if [[ "${CLEAN}" == "1" ]]; then
 fi
 
 patch_tribe_sd_driver
-patch_nbcon_default_prio
+write_tribe_dts
 prepare_config
 
 if [[ "${NO_BUILD}" != "1" ]]; then
